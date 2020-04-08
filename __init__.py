b037b05813d3c6047d6912d6e08a591a8b77a1fc
Luca Vercelli <luca.vercelli.to@gmail.com>
2020-04-08 12:08:17 +0200

    day 0

--- __init__.py
+++ __init__.py
@@ -1,4 +1,7 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from os import system
+from adapt.intent import IntentBuilder
+from mycroft.skills.context import adds_context, removes_context
 
 
 class Dictat(MycroftSkill):
@@ -7,17 +10,33 @@ class Dictat(MycroftSkill):
         self.dictating = False
 
     @intent_file_handler('dictat.start.intent')
-    def start_dictat(self, message):
+    #@adds_context('DoingDictation')
+    def start_dictation(self, message):
         self.dictating = True
         self.speak_dialog('dictat.start')
 
     @intent_file_handler('dictat.stop.intent')
-    def stop_dictat(self, message):
-        self.dictating = False
-        self.speak_dialog('dictat.stop')
+    #@removes_context('DoingDictation')
+    def stop_dictation(self, message):
+        if self.dictating:
+            self.dictating = False
+            self.speak_dialog('dictat.stop')
+        else:
+            self.log.info("Should speak: I'm not doing any dictatation") #TODO
+
+    @intent_handler(IntentBuilder('dictat').require('DoingDictation'))
+    def handle_free_text(self, message):
+        # should check for dictat.stop.intent
+        # self.type_text(message)
+        self.log.info("Dictating: " + str(message))
 
     def stop(self):
-        self.stop_dictat()
+        if self.dictating:
+            self.dictating = False
+            self.speak_dialog('dictat.stop')
+
+    def type_text(text):
+        os.system("xvkbd", "--text", text)
 
 def create_skill():
     return Dictat()
