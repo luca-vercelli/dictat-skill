d6039ac401f07674fc4ac662b29c6e5330a244ce
Luca Vercelli <luca.vercelli.to@gmail.com>
2020-04-08 18:02:56 +0200

    update

--- __init__.py
+++ __init__.py
@@ -2,21 +2,21 @@ from mycroft import MycroftSkill, intent_file_handler, intent_handler
 from os import system
 from adapt.intent import IntentBuilder
 from mycroft.skills.context import adds_context, removes_context
+from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
 
-
-class Dictat(MycroftSkill):
+class Dictat(CommonPlaySkill):
     def __init__(self):
-        MycroftSkill.__init__(self)
+        CommonPlaySkill.__init__(self)
         self.dictating = False
 
     @intent_file_handler('dictat.start.intent')
-    #@adds_context('DoingDictation')
+    @adds_context('DoingDictation')
     def start_dictation(self, message):
         self.dictating = True
         self.speak_dialog('dictat.start')
 
     @intent_file_handler('dictat.stop.intent')
-    #@removes_context('DoingDictation')
+    @removes_context('DoingDictation')
     def stop_dictation(self, message):
         if self.dictating:
             self.dictating = False
@@ -37,6 +37,23 @@ class Dictat(MycroftSkill):
 
     def type_text(text):
         os.system("xvkbd", "--text", text)
+
+    def CPS_match_query_phrase(self, phrase):
+        if self.dictating:
+            # shouldnt be here 
+            return(phrase, CPSMatchLevel.EXACT)
+        elif phrase == "dictate" or phrase == "dictating": #BLEAH
+            return(phrase, CPSMatchLevel.EXACT)
+        else:
+            return None
+
+    def CPS_start(self, phrase, data):
+        if self.dictating:
+            # shouldnt be here 
+            self.type_text(phrase)
+        else:
+            self.dictating = True
+            self.set_context('DoingDictation')
 
 def create_skill():
     return Dictat()
